c03aa8bf306a6ef492874adfcd97e80210e17878
parashar08 <priyanshuparashar08@gmail.com>
2024-09-23 11:30:57 +0530

    add golbal middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,17 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+const corsOptions = {
+    origin: "http://localhost:5173",
+    credentials: true
+}
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "6kb" }));
+app.use(cors(corsOptions));
+app.use(cookieParser());
 
 export default app;
