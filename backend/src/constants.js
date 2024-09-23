555b845944917075e7d5d462660eb136c9049038
parashar08 <priyanshuparashar08@gmail.com>
2024-09-23 11:26:04 +0530

    call db

---
+++ backend/src/constants.js
@@ -0,0 +1,2 @@
+export const DB_NAME = "food-service";
+export const PORT = process.env.PORT || 3000
---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import connectDB from "./database/db.js";
+import app from "./app.js";
+import { PORT } from "./constants.js";
+
+dotenv.config({});
+
+connectDB()
+.then(() => {
+    app.on("error", (err) => {
+        console.error(err);
+    })
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on PORT : ${PORT}`);
+    })
+})
+.catch((err) => {
+    console.error(err);
+    process.exit(1);
+})
