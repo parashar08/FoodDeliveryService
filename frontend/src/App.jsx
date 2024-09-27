5686e74921c4458ec20f22f39d01d3cedfc46bfa
parashar08 <priyanshuparashar08@gmail.com>
2024-09-27 14:25:39 +0530

    create router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,33 @@
 import React from "react"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/shared/Layout"
+import Home from "./pages/Home";
+import Signup from "./components/auth/Signup"
+import Login from "./components/auth/Login"
 
 function App() {
-  return (
-    <>
-      <h1 className="text-red-900 text-2xl">this is app component!</h1>
-    </>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "",
+          element: <Home />
+        },
+        {
+          path: "signup",
+          element: <Signup />
+        },
+        {
+          path: "login",
+          element: <Login />
+        }
+      ]
+    }
+  ])
+
+  return <RouterProvider router={router} />
 }
 
 export default App
